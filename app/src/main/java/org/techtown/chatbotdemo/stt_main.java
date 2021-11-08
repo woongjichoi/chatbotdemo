e8d7210917396cb5f77995632bdae23dd4063f23
Iamjieun <lje980209@gmail.com>
2021-11-08 11:54:29 +0900

    음성인식 자막
    stt_main.java 추가
    activity_main.xml 수정
    AndroidManifest.xml 수정

---
+++ app/src/main/java/org/techtown/chatbotdemo/stt_main.java
@@ -0,0 +1,137 @@
+package org.techtown.chatbotdemo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class stt_main extends AppCompatActivity {
+
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    Button sttBtn;
+    TextView textView;
+    final int PERMISSION = 1 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 퍼미션 체크
+        if ( Build.VERSION.SDK_INT >= 23 ){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET, Manifest.permission.RECORD_AUDIO},PERMISSION);
+        }
+
+        // xml의 버튼과 텍스트 뷰 연결
+        textView = (TextView)findViewById(R.id.sttResult);
+        sttBtn = (Button) findViewById(R.id.sttStart);
+
+        // RecognizerIntent 객체 생성
+        intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
+
+        // 버튼을 클릭 이벤트 - 객체에 Context와 listener를 할당한 후 실행
+        sttBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecognizer=SpeechRecognizer.createSpeechRecognizer(stt_main.this);
+                mRecognizer.setRecognitionListener(listener);
+                mRecognizer.startListening(intent);
+            }
+
+        });
+    }
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getApplicationContext(),"어서오세요.",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {}
+
+        @Override
+        public void onRmsChanged(float rmsdB) {}
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {}
+
+        @Override
+        public void onEndOfSpeech() {}
+
+        // switch case 문이 왜 안으로 들어가는지 모르겠음
+        @Override
+        public void onError(int error) {
+            String message ;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                    case SpeechRecognizer.ERROR_CLIENT:
+                        message = "클라이언트 에러";
+                        break ;
+                        case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                            message = "퍼미션 없음";
+                            break;
+                            case SpeechRecognizer.ERROR_NETWORK:
+                                message = "네트워크 에러";
+                                break;
+                                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                                    message = "네트웍 타임아웃";
+                                    break;
+                                    case SpeechRecognizer.ERROR_NO_MATCH:
+                                        message = "찾을 수 없음";
+                                        break;
+                                        case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                                            message = "RECOGNIZER가 바쁨";
+                                            break;
+                                            case SpeechRecognizer.ERROR_SERVER:
+                                                message = "서버가 이상함";
+                                                break;
+                                                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                                                    message = "말하는 시간초과";
+                                                    break;
+                                                    default:
+                                                        message = "알 수 없는 오류임";
+                                                        break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+
+        public void onResults(Bundle results) {
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            for(int i = 0; i < matches.size() ; i++){
+                textView.setText(matches.get(i));
+            }
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {}
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {}
+    };
+}
