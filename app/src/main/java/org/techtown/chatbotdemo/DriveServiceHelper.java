8d054293bb1ef841e83d7dd27619a2b02abc40f5
woongjichoi <71050595+woongjichoi@users.noreply.github.com>
2021-11-18 01:07:52 +0900

    안드로이드에서 녹음한 음성 파일 Google Drive API 이용하여 구글 드라이브에 올리기 구현

---
+++ app/src/main/java/org/techtown/chatbotdemo/DriveServiceHelper.java
@@ -0,0 +1,47 @@
+package org.techtown.chatbotdemo;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class DriveServiceHelper {
+    private final Executor mExecutor= Executors.newSingleThreadExecutor();
+    private Drive mDriveService;
+
+    public DriveServiceHelper(Drive mDriveService){
+        this.mDriveService=mDriveService;
+    }
+
+    public Task<String> createFileMp3(String filePath){
+        return Tasks.call(mExecutor, ()->{
+            File fileMetaData=new File();
+            fileMetaData.setName("MyMP3File");
+
+            java.io.File file=new java.io.File(filePath);
+
+            FileContent mediaContent=new FileContent("application/3gp", file);
+
+            File myFile=null;
+            try{
+                myFile=mDriveService.files().create(fileMetaData, mediaContent).execute();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            if (myFile==null){
+                throw new IOException("Null result when requesting file creation");
+            }
+
+            return myFile.getId();
+
+        });
+    }
+}
+
+
